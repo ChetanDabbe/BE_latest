9e7b307329d330deaf1077b0a8afaddd375ce854
Chetandabbe <dabbechetan0@com>
2025-04-08 19:57:45 +0530

    Updated

--- backend/app.py
+++ backend/app.py
@@ -105,5 +105,6 @@ def home():
     return "🎉 Flask backend is running on Render!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Render injects PORT env
     app.run(debug=False, host='0.0.0.0', port=port)
+
